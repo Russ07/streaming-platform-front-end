c586f5dce5a613eba969d50f502472fbe5bb8a22
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-06-20 20:03:09 +0100

    header js file html structure ready. connected to a app.js file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,24 @@
 import "./Header.scss";
+import logo from '../../assets/images/BrainFlix-logo.svg';
+import buttonIcon from '../../assets/icons/upload.svg';
+import profileImg from '../../assets/images/Mohan-muruge.jpg';
+
 function Header() {
     return(
         <header className="header">
             <a>
-                <img className="header__logo" src="../../assets/images/brainFlix-logo.svg" />
+                <img className="header__logo" src={logo} />
             </a>
             <div className="header__menu-container">
                 <input className="header__search-bar" type="text" placeholder="Search.."/>
                 <button className="header__button">
-                    <img className="header__button-icon" src="../../assets/icons/upload.svg" />
+                    <img className="header__button-icon" src={buttonIcon}/>
                     UPLOAD
                 </button>
-                <img className="header__profile-img" src="../../assets/images/Mohan-murgue.jpg" />
+                <img className="header__profile-img" src={profileImg} />
             </div>
         </header>
     );
 }
+
+export default Header;
