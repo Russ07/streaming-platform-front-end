633d9f3d372e7488af2034193526a7c9eed61416
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-06-20 23:51:56 +0100

    styling done for header mobile version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,19 @@ import profileImg from '../../assets/images/Mohan-muruge.jpg';
 function Header() {
     return(
         <header className="header">
-            <a>
+            <a className="header__link">
                 <img className="header__logo" src={logo} />
             </a>
-            <div className="header__menu-container">
-                <input className="header__search-bar" type="text" placeholder="Search.."/>
-                <button className="header__button">
-                    <img className="header__button-icon" src={buttonIcon}/>
+            <div className="header__menu-container-mobile">
+                <div className="header__mobile-wrapper">
+                <input className="header__search-bar header__search-bar-mobile" type="text" placeholder="&#xf002;  Search"/>
+                <img className="header__profile-img-mobile" src={profileImg} />
+                </div>
+                
+                <button className="header__button header__button-mobile">
+                    <img className="header__button-icon header__button-icon-mobile" src={buttonIcon}/>
                     UPLOAD
                 </button>
-                <img className="header__profile-img" src={profileImg} />
             </div>
         </header>
     );
