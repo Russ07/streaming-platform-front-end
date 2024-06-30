4df542db298c478ef6a425fb2f940cfc2ae71f86
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-06-30 10:53:22 +0100

    video tag created and js file imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.js';
+import Hero from './components/Video/Video.js'
 function App() {
   return (
     <>
   <Header />
+  <Hero />
     </>
   );
 }
---
+++ src/components/Video/Video.js
@@ -0,0 +1,11 @@
+import './Video.scss';
+
+function Hero() {
+    return(
+        <>
+        <video className='video'></video>
+        </>
+    );
+}
+
+export default Hero;
