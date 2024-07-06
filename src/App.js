c0f581ebe27af49f0702292fa5116dc9216d5b10
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-07-06 21:50:22 +0100

    structure for description created and fixed connection for app.scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import "./App.scss"
 import Header from './components/Header/Header.js';
-import Hero from './components/Video/Video.js'
+import Hero from './components/Video/Video.js';
+import VideoDescription from './components/VideoDescription/VideoDescription.js';
 function App() {
   return (
     <>
   <Header />
   <Hero />
+  <VideoDescription/>  
     </>
   );
 }
