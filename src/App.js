363dd2364cb3cf2a98d5ef3b2ac825373f64d661
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-07-07 19:33:39 +0100

    sample of connectiong json file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header.js';
 import Hero from './components/Video/Video.js';
 import VideoDescription from './components/VideoDescription/VideoDescription.js';
 import Comments from './components/Comments/Comments.js';
+import VideoElement from './components/VideoElement/VideoElement.js';
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
   <div className='description-comments-wrapper'>
   <VideoDescription/>
   <Comments/>
+  <VideoElement/>
   </div>  
     </>
   );
---
+++ src/components/VideoElement/VideoElement.js
@@ -0,0 +1,24 @@
+import "./VideoElement.scss";
+import data from '../../data/video-details.json';
+
+console.log({data});
+
+function VideoElement() {
+    const arrayData = {data};
+    console.log(arrayData[0]);
+    return (
+      <>
+        <section className="card-list">
+            {cardContent.map((content, index) => (
+                <Card 
+                    key={index} 
+                    title={content.title} 
+                    content={content.content}
+                />
+            ))}
+        </section>
+      </>
+    );
+  }
+  
+  export default VideoElement;
