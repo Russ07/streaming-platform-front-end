83f2031735253f361a41b032c3c7574d000c399f
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-07-21 11:59:49 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./App.scss";
 import listData from './data/video-details.json'
@@ -6,14 +5,13 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Video/Video";
 import VideoDescription from "./components/VideoDescription/VideoDescription";
 import Comments from "./components/Comments/Comments";
-import VideoElement from "./components/VideoElement/VideoElement";
 import VideoList from "./components/VideoList/VideoList";
 import { useState } from "react";
 function App() {
 
   // finds video for hero elemet
   function findObject(id){
-    const currentObject = listData.find((video) => video.id == id);
+    const currentObject = listData.find((video) => video.id === id);
     console.log(currentObject)
     return currentObject;
   }
@@ -28,7 +26,7 @@ function App() {
   console.log(currentVideo.id)
 
   // creat a list without element that been clicked
-  const suggestedVideos = listData.filter((video) => video.id != currentVideo.id);
+  const suggestedVideos = listData.filter((video) => video.id !== currentVideo.id);
   console.log(suggestedVideos)
 
   return (
